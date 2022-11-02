4cd23b1c304f3243701e20b13c583a6e5625bb52
Florian LEMAITRE <flemaitre@aneo.fr>
2022-11-02 19:02:04 +0100

    Enumerable extensions

---
+++ csharp/src/Chunk.cs
@@ -0,0 +1,93 @@
+﻿// This file is part of the ArmoniK project
+//
+// Copyright (C) ANEO, 2022-2022.All rights reserved.
+//   W.Kirschenmann   <wkirschenmann@aneo.fr>
+//   J.Gurhem         <jgurhem@aneo.fr>
+//   D.Dubuc          <ddubuc@aneo.fr>
+//   L.Ziane Khodja   <lzianekhodja@aneo.fr>
+//   F.Lemaitre       <flemaitre@aneo.fr>
+//   S.Djebbar        <sdjebbar@aneo.fr>
+//   J.Fonseca        <jfonseca@aneo.fr>
+//   D.Brasseur       <dbrasseur@aneo.fr>
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Diagnostics;
+
+namespace ArmoniK.Utils;
+
+internal static class Chunk
+{
+  // Implementation of the AsChunked function
+  // Original source code : https://github.com/dotnet/runtime/blob/main/src/libraries/System.Linq/src/System/Linq/Chunk.cs
+  internal static IEnumerable<TSource[]> Iterator<TSource>(IEnumerable<TSource> source,
+                                                           int                  size)
+  {
+    using var e = source.GetEnumerator();
+
+    // Before allocating anything, make sure there's at least one element.
+    if (e.MoveNext())
+    {
+      // Now that we know we have at least one item, allocate an initial storage array. This is not
+      // the array we'll yield.  It starts out small in order to avoid significantly overallocating
+      // when the source has many fewer elements than the chunk size.
+      var arraySize = Math.Min(size,
+                               4);
+      int i;
+      do
+      {
+        var array = new TSource[arraySize];
+
+        // Store the first item.
+        array[0] = e.Current;
+        i        = 1;
+
+        if (size != array.Length)
+        {
+          // This is the first chunk. As we fill the array, grow it as needed.
+          for (; i < size && e.MoveNext(); i++)
+          {
+            if (i >= array.Length)
+            {
+              arraySize = (int)Math.Min((uint)size,
+                                        2 * (uint)array.Length);
+              Array.Resize(ref array,
+                           arraySize);
+            }
+
+            array[i] = e.Current;
+          }
+        }
+        else
+        {
+          // For all but the first chunk, the array will already be correctly sized.
+          // We can just store into it until either it's full or MoveNext returns false.
+          var local = array; // avoid bounds checks by using cached local (`array` is lifted to iterator object as a field)
+          Debug.Assert(local.Length == size);
+          for (; (uint)i < (uint)local.Length && e.MoveNext(); i++)
+          {
+            local[i] = e.Current;
+          }
+        }
+
+        if (i != array.Length)
+        {
+          Array.Resize(ref array,
+                       i);
+        }
+
+        yield return array;
+      } while (i >= size && e.MoveNext());
+    }
+  }
+}
---
+++ csharp/src/EnumerableExt.cs
@@ -0,0 +1,107 @@
+﻿// This file is part of the ArmoniK project
+//
+// Copyright (C) ANEO, 2022-2022.All rights reserved.
+//   W.Kirschenmann   <wkirschenmann@aneo.fr>
+//   J.Gurhem         <jgurhem@aneo.fr>
+//   D.Dubuc          <ddubuc@aneo.fr>
+//   L.Ziane Khodja   <lzianekhodja@aneo.fr>
+//   F.Lemaitre       <flemaitre@aneo.fr>
+//   S.Djebbar        <sdjebbar@aneo.fr>
+//   J.Fonseca        <jfonseca@aneo.fr>
+//   D.Brasseur       <dbrasseur@aneo.fr>
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using JetBrains.Annotations;
+
+namespace ArmoniK.Utils;
+
+public static class EnumerableExt
+{
+  /// <summary>
+  ///   Convert an enumerable into a list, if it is not already a list
+  ///   Beware that the return list may or may not be a reference to the input enumerable
+  /// </summary>
+  /// <param name="enumerable">The enumerable to convert into a list</param>
+  /// <typeparam name="T">Type of the elements</typeparam>
+  /// <returns>A list containing the same elements as the input enumerable</returns>
+  [PublicAPI]
+  public static IList<T> AsList<T>(this IEnumerable<T>? enumerable)
+  {
+    if (enumerable is null)
+    {
+      return Array.Empty<T>();
+    }
+
+    return enumerable as IList<T> ?? enumerable.ToList();
+  }
+
+  /// <summary>
+  ///   Convert an enumerable into a collection, if it is not already a collection
+  ///   Beware that the return collection may or may not be a reference to the input enumerable
+  /// </summary>
+  /// <param name="enumerable">The enumerable to convert into a collection</param>
+  /// <typeparam name="T">Type of the elements</typeparam>
+  /// <returns>A collection containing the same elements as the input enumerable</returns>
+  [PublicAPI]
+  public static ICollection<T> AsCollection<T>(this IEnumerable<T>? enumerable)
+  {
+    if (enumerable is null)
+    {
+      return Array.Empty<T>();
+    }
+
+    return enumerable as ICollection<T> ?? enumerable.ToList();
+  }
+
+  /// <summary>
+  ///   Split the elements of a sequence into chunks of size at most <paramref name="size" />.
+  /// </summary>
+  /// <remarks>
+  ///   Every chunk except the last will be of size <paramref name="size" />.
+  ///   The last chunk will contain the remaining elements and may be of a smaller size.
+  /// </remarks>
+  /// <param name="source">
+  ///   An <see cref="IEnumerable{T}" /> whose elements to chunk.
+  /// </param>
+  /// <param name="size">
+  ///   Maximum size of each chunk.
+  /// </param>
+  /// <typeparam name="TSource">
+  ///   The type of the elements of source.
+  /// </typeparam>
+  /// <returns>
+  ///   An <see cref="IEnumerable{T}" /> that contains the elements the input sequence split into chunks of size
+  ///   <paramref name="size" />.
+  /// </returns>
+  /// <exception cref="ArgumentOutOfRangeException">
+  ///   <paramref name="size" /> is below 1.
+  /// </exception>
+  [PublicAPI]
+  public static IEnumerable<TSource[]> AsChunked<TSource>(this IEnumerable<TSource>? source,
+                                                          int                        size)
+  {
+    if (source is null)
+    {
+      return Array.Empty<TSource[]>();
+    }
+
+    if (size < 1)
+    {
+      throw new ArgumentOutOfRangeException(nameof(size));
+    }
+
+    return Chunk.Iterator(source,
+                          size);
+  }
+}
