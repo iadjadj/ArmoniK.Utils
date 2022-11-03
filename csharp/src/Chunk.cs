904cc38914295f02a78e51136ac0bab0bc38e1d1
Florian LEMAITRE <flemaitre@aneo.fr>
2022-11-03 10:27:35 +0100

    Aneo implementation of Chunk

--- csharp/src/Chunk.cs
+++ csharp/src/Chunk.cs
@@ -27,59 +27,30 @@ internal static class Chunk
   {
     using var e = source.GetEnumerator();
 
-    // Before allocating anything, make sure there's at least one element.
-    if (e.MoveNext())
+    // TODO: defer allocation until we are sure to have at least one element
+    var buffer = new TSource[size];
+
+    // Bad, I know
+    while (true)
     {
-      // Now that we know we have at least one item, allocate an initial storage array. This is not
-      // the array we'll yield.  It starts out small in order to avoid significantly overallocating
-      // when the source has many fewer elements than the chunk size.
-      var arraySize = Math.Min(size,
-                               4);
-      int i;
+      var i = 0;
       do
       {
-        var array = new TSource[arraySize];
-
-        // Store the first item.
-        array[0] = e.Current;
-        i        = 1;
-
-        if (size != array.Length)
+        // This is the exit point
+        if (!e.MoveNext())
         {
-          // This is the first chunk. As we fill the array, grow it as needed.
-          for (; i < size && e.MoveNext(); i++)
+          if (i > 0)
           {
-            if (i >= array.Length)
-            {
-              arraySize = (int)Math.Min((uint)size,
-                                        2 * (uint)array.Length);
-              Array.Resize(ref array,
-                           arraySize);
-            }
-
-            array[i] = e.Current;
+            Array.Resize(ref buffer, i);
+            yield return buffer;
           }
-        }
-        else
-        {
-          // For all but the first chunk, the array will already be correctly sized.
-          // We can just store into it until either it's full or MoveNext returns false.
-          var local = array; // avoid bounds checks by using cached local (`array` is lifted to iterator object as a field)
-          Debug.Assert(local.Length == size);
-          for (; (uint)i < (uint)local.Length && e.MoveNext(); i++)
-          {
-            local[i] = e.Current;
-          }
+
+          yield break;
         }
 
-        if (i != array.Length)
-        {
-          Array.Resize(ref array,
-                       i);
-        }
-
-        yield return array;
-      } while (i >= size && e.MoveNext());
+        buffer[i] =  e.Current;
+        i         += 1;
+      } while (i < size);
     }
   }
 }
