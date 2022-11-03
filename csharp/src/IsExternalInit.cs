849c83ba30e7b826ed56c77f58ab314a011db96e
Florian LEMAITRE <flemaitre@aneo.fr>
2022-11-03 23:24:49 +0100

    Better IsExternalInit

--- csharp/src/IsExternalInit.cs
+++ csharp/src/IsExternalInit.cs
@@ -1,24 +1,23 @@
-﻿// This file is part of the ArmoniK project
-//
-// Copyright (C) ANEO, 2022-2022. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License")
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+// https://github.com/dotnet/roslyn/issues/45510
+
+#if NETSTANDARD2_0 || NETSTANDARD2_1 || NETCOREAPP2_0 || NETCOREAPP2_1 || NETCOREAPP2_2 || NETCOREAPP3_0 || NETCOREAPP3_1 || NET45 || NET451 || NET452 || NET46 || NET461 || NET462 || NET47 || NET471 || NET472 || NET48
 
 using System.ComponentModel;
 
-// ReSharper disable CheckNamespace
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+  /// <summary>
+  ///   Reserved to be used by the compiler for tracking metadata.
+  ///   This class should not be used by developers in source code.
+  /// </summary>
+  [EditorBrowsable(EditorBrowsableState.Never)]
+  internal static class IsExternalInit
+  {
+  }
+}
 
-namespace System.Runtime.CompilerServices;
-
-[EditorBrowsable(EditorBrowsableState.Never)]
-public record IsExternalInit;
+#endif
