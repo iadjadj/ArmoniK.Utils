f0a5af01e54af2713b719c13ce61d094649ecede
Florian LEMAITRE <flemaitre@aneo.fr>
2022-11-04 12:12:44 +0100

    Cleaner Chunk

--- csharp/src/Chunk.cs
+++ csharp/src/Chunk.cs
@@ -16,88 +16,59 @@
 
 using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 
 namespace ArmoniK.Utils;
 
 internal static class Chunk
 {
-  private struct ArrayGrower<T>
-  {
-    private T[] array_;
-    private int count_;
-    private readonly int maxSize_;
-    public ArrayGrower(int maxSize, int? initialCapacity = null)
-    {
-      maxSize_ = maxSize;
-      count_ = 0;
-      array_ = initialCapacity is null ? Array.Empty<T>() : new T[initialCapacity.Value];
-    }
-    internal T[] InternalArray => array_;
-    public int Count => count_;
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void AddAndGrow(T element)
-    {
-      var length = array_.Length;
-      if (length != maxSize_ && count_ == length)
-      {
-        var newLength = Math.Min(Math.Max(length + length /2, 4), maxSize_);
-        Array.Resize(ref array_, newLength);
-      }
-      array_[count_++] = element; // Will intentionally throw if more elements than size are added
-    }
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void Add(T element)
-    {
-      array_[count_++] = element; // Will intentionally throw if more elements than size are added
-    }
-    public void Clear()
-    {
-      count_ = 0;
-    }
-    public void Trimm()
-    {
-      if (count_ != array_.Length)
-      {
-        Array.Resize(ref array_, count_);
-      }
-    }
-  }
   // Implementation of the AsChunked function
   // Original source code : https://github.com/dotnet/runtime/blob/main/src/libraries/System.Linq/src/System/Linq/Chunk.cs
   internal static IEnumerable<TSource[]> Iterator<TSource>(IEnumerable<TSource> source,
-                                                            int size)
+                                                           int                  size)
   {
     using var e = source.GetEnumerator();
 
-    var buffer = new ArrayGrower<TSource>(size); // cheap allocation can be afforded
+    var buffer = Array.Empty<TSource>();
+    int bufferSize;
 
-    { // first chunk
-      for (var i = 0; i < size && e.MoveNext(); ++i)
+    {
+      // first chunk
+      for (bufferSize = 0; bufferSize < size && e.MoveNext(); ++bufferSize)
       {
-        buffer.AddAndGrow(e.Current);
+        if (bufferSize >= buffer.Length)
+        {
+          var newLength = Math.Min(Math.Max(buffer.Length + buffer.Length / 2,
+                                            4),
+                                   size);
+          Array.Resize(ref buffer,
+                       newLength);
+        }
+
+        buffer[bufferSize] = e.Current;
       }
     }
     // buffer is now the right size here
 
     while (true) // other chunks
     {
-      if (buffer.Count != size) // Incomplete chunk
+      if (bufferSize != size) // Incomplete chunk
       {
         // chunk is not empty, and must be trimmed and return
-        if (buffer.Count > 0)
+        if (bufferSize > 0)
         {
-          buffer.Trimm();
-          yield return buffer.InternalArray;
+          Array.Resize(ref buffer,
+                       bufferSize);
+          yield return buffer;
         }
+
         yield break;
       }
-      yield return buffer.InternalArray; // chunk is complete and a new storage is required
 
-      buffer.Clear();
-      for (var i = 0; i < size && e.MoveNext(); ++i)
+      yield return buffer; // chunk is complete and a new storage is required
+
+      for (bufferSize = 0; bufferSize < size && e.MoveNext(); ++bufferSize)
       {
-        buffer.Add(e.Current);
+        buffer[bufferSize] = e.Current;
       }
     }
   }
